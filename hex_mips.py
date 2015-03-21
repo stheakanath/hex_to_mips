67bc1212a259f74fdf170c617ae6cc333e6d3ad7
Sony Theakanath <stheakanath@gmail.com>
2015-03-21 14:50:57 -0700

    better documentation

--- hex_mips.py
+++ hex_mips.py
@@ -1,18 +1,11 @@
-'''
-This is a hex to MIPS converter. Handles binary converstion and the stuff.
-Takes in a text file and outputs the likes
+# Hex to MIPS converter. 
+# Takes in a hex compiled program and spits out the MIPS equilivant
+# Input: Machine Language Compiled File
+# Output: TAL MIPS Language
 
-Prereqs: HEX file must be in TAL MIPS (Total Assembly Language), not Pseudo
-Language
-Output: TAL MIPS in readable format
-For CS 61C.
-'''
-
-'''
-The key values signify OPCODE, which are in decimal format, not hex
-which are given by MIPS green sheet. 
-'''
-
+# Dictionaries: Follows 32 bit MIPS format from Berkeley's MIPS Green Sheet
+# r_type_fx : R-Type Funct codes; i_type : I-Type Opcodes; j_type : J-Type Opcodes 
 r_type_fx = {'add' : 32, 'addu' : 8, 'and' : 36, 'jr' : 8, 'nor' : 39, 'or' : 37, 'slt' : 42, 'sltu' : 43, 'sll': 0, 'srl' : 2, 'sub' : 34, 'subu' : 35}
 i_type = {'addi' : 8, 'addiu' : 9, 'andi' : 12, 'beq' : 4, 'bne' : 5, 'lbu' : 36, 'lhu' : 37, 'll' : 48, 'lui' : 15, 'lw' : 35, 'ori' : 13, 'slti' : 10, 'sltiu' : 11, 'sb' : 40, 'sc' : 56, 'sh' : 41, 'sw' : 43}
 j_type = {'j' : 2, 'jal' : 3}
+
