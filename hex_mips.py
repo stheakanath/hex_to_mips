87b39d18f875a1092ce2adf97686652b4cb28135
Sony Theakanath <stheakanath@gmail.com>
2015-03-21 15:01:57 -0700

    added read line capability and starting converting to binary function

--- hex_mips.py
+++ hex_mips.py
@@ -2,6 +2,7 @@
 # Takes in a hex compiled program and spits out the MIPS equilivant
 # Input: Machine Language Compiled File
 # Output: TAL MIPS Language
+# Author: Kuriakose Sony Theakanath
 
 # Dictionaries: Follows 32 bit MIPS format from Berkeley's MIPS Green Sheet
 # r_type_fx : R-Type Funct codes; i_type : I-Type Opcodes; j_type : J-Type Opcodes 
@@ -9,3 +10,14 @@ r_type_fx = {'add' : 32, 'addu' : 8, 'and' : 36, 'jr' : 8, 'nor' : 39, 'or' : 37
 i_type = {'addi' : 8, 'addiu' : 9, 'andi' : 12, 'beq' : 4, 'bne' : 5, 'lbu' : 36, 'lhu' : 37, 'll' : 48, 'lui' : 15, 'lw' : 35, 'ori' : 13, 'slti' : 10, 'sltiu' : 11, 'sb' : 40, 'sc' : 56, 'sh' : 41, 'sw' : 43}
 j_type = {'j' : 2, 'jal' : 3}
 
+def read_file(fname):
+	lines = open(fname).read().splitlines()
+	return lines
+
+def convert_to_binary(list):
+	"""Converts hex line to binary, takes in a list of hex commands
+	and returns a list of those lines converted to binary.
+
+	>>> l = convert_to_binary()
+
+	"""
